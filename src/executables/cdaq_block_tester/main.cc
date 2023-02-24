765777ff42af768ccbd97827331fdbd0b6d71cfd
Xinxin Mei <xmei@jlab.org>
2023-02-24 03:21:48 -0500

    Init jblock_tester
    Compiled and ran

---
+++ src/executables/cdaq_block_tester/main.cc
@@ -0,0 +1,45 @@
+// Refer to JANA2 BlockExample
+
+#include <JANA/JApplication.h>
+#include <JANA/Engine/JTopologyBuilder.h>
+#include <JANA/Engine/JBlockSourceArrow.h>
+#include <JANA/Engine/JBlockDisentanglerArrow.h>
+#include <JANA/Engine/JEventProcessorArrow.h>
+
+#include "EVIOBlockedEvent.h"
+#include "EVIOBlockedEventSource.h"
+#include "EVIOBlockProcessor.h"
+
+
+int main() {
+
+    JApplication app;
+    auto topology = app.GetService<JTopologyBuilder>()->create_empty();
+
+    auto source = new EVIOBlockedEventSource;
+    auto processor = new EVIOBlockProcessor();
+
+    auto block_queue = new JMailbox<EVIOBlockedEvent *>;
+    auto event_queue = new JMailbox <std::shared_ptr<JEvent>>;
+
+    auto block_source_arrow = new JBlockSourceArrow<EVIOBlockedEvent>("block_source", source, block_queue);
+    auto block_disentangler_arrow = new JBlockDisentanglerArrow<EVIOBlockedEvent>(
+            "block_disentangler", source, block_queue, event_queue, topology->event_pool);
+    auto processor_arrow = new JEventProcessorArrow("processors", event_queue, nullptr, topology->event_pool);
+
+    processor_arrow->add_processor(processor);
+
+    topology->arrows.push_back(block_source_arrow);
+    topology->arrows.push_back(block_disentangler_arrow);
+    topology->arrows.push_back(processor_arrow);
+
+    topology->sources.push_back(block_source_arrow);
+    topology->sinks.push_back(processor_arrow);
+
+    block_source_arrow->attach(block_disentangler_arrow);
+    block_disentangler_arrow->attach(processor_arrow);
+
+    app.SetParameterValue("log:trace", "JWorker");
+    app.Run(true);
+}
+
