86d75693dee88de6273585ed6f6a212e28b9ad79
Dmitry Romanov <romanovda@gmail.com>
2023-03-07 23:19:22 -0500

    Remove WITH_STATIC_LIB in plugin cmake

--- cmake/jana_plugin.cmake
+++ cmake/jana_plugin.cmake
@@ -1,17 +1,6 @@
 # Common macro to add plugins
 macro(plugin_add _name)
-
     project(${_name}_project)
-
-    # Check if build with library
-    foreach(arg IN ITEMS ${ARGN})
-        if(${arg} STREQUAL "WITH_STATIC_LIBRARY")
-            set(${_name}_WITH_STATIC_LIB ON)
-        endif()
-        if(${arg} STREQUAL "WITH_STATIC_LIB")       # alternative
-            set(${_name}_WITH_STATIC_LIB ON)
-        endif()
-    endforeach()
 
     # Include JANA by default
     find_package(JANA REQUIRED)
@@ -32,52 +21,22 @@ macro(plugin_add _name)
     target_include_directories(${_name}_plugin SYSTEM PUBLIC ${ROOT_INCLUDE_DIRS} )
     target_include_directories(${_name}_plugin PUBLIC ${fmt_DIR}/../../../include)
     set_target_properties(${_name}_plugin PROPERTIES PREFIX "" OUTPUT_NAME "${_name}" SUFFIX ".so")
-    target_link_libraries(${_name}_plugin ${JANA_LIB} spdlog::spdlog)
-    target_link_libraries(${_name}_plugin ${JANA_LIB} fmt::fmt)
+    target_link_libraries(${_name}_plugin ${JANA_LIB} spdlog::spdlog fmt::fmt)
 
     # Install plugin
     install(TARGETS ${_name}_plugin DESTINATION ${PLUGIN_OUTPUT_DIRECTORY})
-
-
-    if(${_name}_WITH_STATIC_LIB)
-        # Define library
-        add_library(${_name}_library STATIC "")
-	    target_include_directories(${_name}_library PUBLIC ${CMAKE_SOURCE_DIR}/src)
-        target_include_directories(${_name}_library SYSTEM PUBLIC ${JANA_INCLUDE_DIR} )
-        target_include_directories(${_name}_library PUBLIC ${fmt_DIR}/../../../include)
-        set_target_properties(${_name}_library PROPERTIES PREFIX "lib" OUTPUT_NAME "${_name}" SUFFIX ".a")
-        target_link_libraries(${_name}_library ${JANA_LIB} spdlog::spdlog)
-        target_link_libraries(${_name}_library ${JANA_LIB} fmt::fmt)
-
-        # Install plugin
-        install(TARGETS ${_name}_library DESTINATION ${PLUGIN_LIBRARY_OUTPUT_DIRECTORY})
-    endif()     # WITH_STATIC_LIB
 endmacro()
 
 
 # target_link_libraries for both a plugin and a library
 macro(plugin_link_libraries _name)
-
-    #foreach(arg IN ITEMS ${ARGN})
-    #    target_link_libraries(${_name}_plugin ${arg})
     target_link_libraries(${_name}_plugin ${ARGN})
-    #endforeach()
-
-
-
-    if(${_name}_WITH_STATIC_LIB)
-        target_link_libraries(${_name}_library ${ARGN})
-    endif()     # WITH_STATIC_LIB
 endmacro()
 
 
 # target_include_directories for both a plugin and a library
 macro(plugin_include_directories _name)
     target_include_directories(${_name}_plugin  ${ARGN})
-
-    if(${_name}_WITH_STATIC_LIB)
-        target_include_directories(${_name}_library ${ARGN})
-    endif()     # WITH_STATIC_LIB
 endmacro()
 
 
@@ -88,16 +47,6 @@ macro(plugin_sources _name)
 
     # Add sources to plugin
     target_sources(${_name}_plugin PRIVATE ${SOURCES})
-
-    if(${_name}_WITH_STATIC_LIB)
-        # Library don't need <plugin_name>.cc in library
-        set(PLUGIN_CC_FILE ${_name}.cc)
-        get_filename_component(PLUGIN_CC_FILE "${CMAKE_CURRENT_LIST_DIR}/${PLUGIN_CC_FILE}" ABSOLUTE)
-        list(REMOVE_ITEM SOURCES ${PLUGIN_CC_FILE})
-
-        # Add sources to library
-        target_sources(${_name}_library PRIVATE ${SOURCES})
-    endif()     # WITH_STATIC_LIB
 endmacro()
 
 # The macro grabs sources as *.cc *.cpp *.c and headers as *.h *.hh *.hpp
@@ -119,25 +68,6 @@ macro(plugin_glob_all _name)
     #Add correct headers installation
     # Install headers for plugin
     install(FILES ${HEADER_FILES} DESTINATION include/${PLUGIN_RELATIVE_PATH})
-
-    if(${_name}_WITH_STATIC_LIB)
-        # Library don't need <plugin_name>.cc but Plugin does
-        set(PLUGIN_CC_FILE ${_name}.cc)
-
-        # Make the path absolute as GLOB files will be absolute paths
-        get_filename_component(PLUGIN_CC_FILE_ABS "${CMAKE_CURRENT_LIST_DIR}/${PLUGIN_CC_FILE}" ABSOLUTE)
-
-        # Remove plugin.cc file from libraries
-        list(REMOVE_ITEM LIB_SRC_FILES ${PLUGIN_CC_FILE_ABS})
-
-        # >oO Debug output if needed
-        if(${EICRECON_VERBOSE_CMAKE})
-            message(STATUS "plugin_glob_all:${_name}: LIB_SRC_FILES    ${LIB_SRC_FILES}")
-        endif()
-
-        # Finally add sources to library
-        target_sources(${_name}_library PRIVATE ${LIB_SRC_FILES})
-    endif()     # WITH_STATIC_LIB
 
     # >oO Debug output if needed
     if(${EICRECON_VERBOSE_CMAKE})
