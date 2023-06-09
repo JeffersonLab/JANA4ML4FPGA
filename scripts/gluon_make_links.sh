abd0c397a820772ca05ef3cbb808caa40f11e815
David Lawrence <davidl@jlab.org>
2023-06-09 02:09:06 -0400

    Proper gitignore for analysis scripts update

---
+++ scripts/gluon_make_links.sh
@@ -0,0 +1,7 @@
+
+#!/usr/bin/bash
+ln -s /gluonraid3/data4/rawdata/trd/LOG LOG
+ln -s /home/hdtrdops/ROOT ROOT
+ln -s /home/hdtrdops/DATA DATA
+ln -s /home/hdtrdops/soft_ml4fpga/setup_env.sh setup_env.sh
+ln -s /home/hdtrdops/soft_ml4fpga/setup_env.csh setup_env.csh
