69bc82765eed91928455bfc8a63b3d3af61f59de
Dmitry Romanov <romanovda@gmail.com>
2023-06-18 16:22:37 -0400

    RAII directory switch implemented

---
+++ src/plugins/gemrecon/ApvDecodingResult.h
@@ -0,0 +1,9 @@
+//
+// Created by romanov on 6/17/2023.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#ifndef JANA4ML4FPGA_APVDECODINGRESULT_H
+#define JANA4ML4FPGA_APVDECODINGRESULT_H
+
+#endif //JANA4ML4FPGA_APVDECODINGRESULT_H
---
+++ src/plugins/gemrecon/PlanePeak.h
@@ -0,0 +1,9 @@
+//
+// Created by romanov on 6/18/2023.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#ifndef JANA4ML4FPGA_PLANEPEAK_H
+#define JANA4ML4FPGA_PLANEPEAK_H
+
+#endif //JANA4ML4FPGA_PLANEPEAK_H
