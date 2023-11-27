d0656d6113824135329b08ba83c9e78c67bb7b2a
Dmitry Romanov <romanovda@gmail.com>
2023-11-27 01:37:06 -0500

    Better DQM Peak finding display

---
+++ src/plugins/gemrecon/PeakFactory.h
@@ -0,0 +1,9 @@
+//
+// Created by romanov on 11/25/2023.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+
+#ifndef PEAKFACTORY_H
+#define PEAKFACTORY_H
+
+#endif //PEAKFACTORY_H
